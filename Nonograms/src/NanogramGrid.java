9a2ab44c05c7958a0358b732209009722585908d
Tzafir <168145165+Tzafir@users.noreply.github.com>
2024-05-16 10:32:44 +0100

    Added Getter methods for NanogramGrid
    Added Getter methods that will help to display the numbers at the top and left of the image that represents the number of pixels that are shaded on the respective row or column.

--- Nonograms/src/NanogramGrid.java
+++ Nonograms/src/NanogramGrid.java
@@ -2,13 +2,67 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class NanogramGrid extends JFrame {
+    //Atributes for the class NanogramGrid
+    //Also includes getter methods
     private final int row;
     private final int col;
     private final JButton[][] gridButtons;
-    private final boolean[][] solutionNano;
+    private final int[][] solutionNono;
+    private String[] getRowNumbers(int[][] solutionNono){
+        String[] rowNumber = new String[solutionNono.length];
+        for(int i = 0;i<solutionNono.length;i++){
+            String rowNumbers = "";
+            int count = 0;
+            for(int j = 0;j<solutionNono[i].length;j++){
+                
+                if (solutionNono[i][j] != 0){ //we Check that the element is non-zero and add it to the counter
+                    count++;
+                }else{
+                    if(count>0){ //If the element is zero then we add the count to the variable rowNumbers.
+                        rowNumbers += count ;
+                        count = 0;
+                    }
+                }
+            }
+            if (count>0){
+                rowNumbers += count;//In case the last element is not non-zero.
+            }
+            if (rowNumbers.isEmpty()) {
+                rowNumbers = "0";
+            }
+            rowNumber[i] = rowNumbers;
+        }
+        return rowNumber;
+    }
+    private String[] getColumnNumbers(int[][] solutionNano){
+        String[] colNumber = new String[solutionNono.length];
+        for(int j = 0;j<solutionNono[0].length;j++){
+            String colNumbers = "";
+            int count = 0;
+            for(int i = 0;i<solutionNono.length;i++){
+                
+                if (solutionNono[i][j] != 0){
+                    count++;
+                }else{
+                    if(count>0){
+                        colNumbers += count ;
+                        count = 0;
+                    }
+                }
+            }
+            if (count>0){
+                colNumbers += count;
+            }
+            if (colNumbers.isEmpty()) {
+                colNumbers = "0";
+            }
+            colNumber[j] = colNumbers;
+        }
+        return colNumber;
+    }
     // constructor
-    public NanogramGrid(boolean[][] solution) {
-        this.solutionNano = solution;
+    public NanogramGrid(int [][] solution) {
+        this.solutionNono = solution;
         this.row = solution.length;
         this.col = solution[0].length;
 
