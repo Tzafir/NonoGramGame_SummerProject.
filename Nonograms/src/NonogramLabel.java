73b78f952de59825e538520bed4360b9a7a8aa60
Tzafir <168145165+Tzafir@users.noreply.github.com>
2024-05-17 13:07:45 +0100

    Updated getting the row and column number functions to include two colours (red and black)

--- Nonograms/src/NonogramLabel.java
+++ Nonograms/src/NonogramLabel.java
@@ -1,55 +1,87 @@
-public class NonogramLabel {
-    public static String[] getRowNumbers(int[][] solutionNono) {
-        String[] rowNumber = new String[solutionNono.length];
+public class NonogramLabel{
+public static String[] getRowNumbers(int[][] solutionNono) {
+    String[] rowNumber = new String[solutionNono.length];
+    for (int i = 0; i < solutionNono.length; i++) {
+        String rowNumbers = "";
+        int blackCount = 0;
+        int redCount = 0;
+        for (int j = 0; j < solutionNono[i].length; j++) { // For loop going through the Array
+            if (solutionNono[i][j] == 1) {  ////Every time a colours come in consecutively//
+                if (redCount > 0) {          //it's counters will go up until a zero or another color is seen.//
+                    rowNumbers += redCount + "R";
+                    redCount = 0;
+                }
+                blackCount++;
+            } else if (solutionNono[i][j] == 2) {
+                if (blackCount > 0) {
+                    rowNumbers += blackCount + "B";
+                    blackCount = 0;
+                }
+                redCount++;
+            } else if (solutionNono[i][j] == 0) {
+                if (blackCount > 0 ) {
+                    rowNumbers += blackCount + "B ";
+                    blackCount = 0;
+                }
+                if (redCount > 0 ) {
+                    rowNumbers += redCount + "R ";
+                    redCount = 0;
+                }
+            }
+        }
+        if (blackCount > 0) {
+            rowNumbers += blackCount + "B";
+        } else if (redCount > 0) {
+            rowNumbers += redCount + "R";
+        }
+        if (rowNumbers.isEmpty()) {
+            rowNumbers = "0";
+        }
+        rowNumber[i] = rowNumbers;
+    }
+    return rowNumber;
+}
+//Same as Row but get's Numbers for column
+public static String[] getColumnNumbers(int[][] solutionNono) {
+    String[] colNumber = new String[solutionNono[0].length];
+    for (int j = 0; j < solutionNono[0].length; j++) {
+        String colNumbers = "";
+        int blackCount = 0;
+        int redCount = 0;
         for (int i = 0; i < solutionNono.length; i++) {
-            String rowNumbers = "";
-            int count = 0;
-            for (int j = 0; j < solutionNono[i].length; j++) {
-
-                if (solutionNono[i][j] != 0) { // we Check that the element is non-zero and add it to the counter
-                    count++;
-                } else {
-                    if (count > 0) { // If the element is zero then we add the count to the variable rowNumbers.
-                        rowNumbers += count + " ";
-                        count = 0;
-                    }
+            if(solutionNono[i][j] == 1) {
+                if (redCount > 0){
+                    colNumbers += redCount +"R";
+                    redCount = 0;
+                }
+                blackCount++;
+            }else if(solutionNono[i][j] == 2){
+                if(blackCount > 0){
+                    colNumbers += blackCount +"B";
+                    blackCount = 0;
+                }
+                redCount++;
+            }else if(solutionNono[i][j] == 0){
+                if(blackCount > 0){
+                    colNumbers += blackCount +"B ";
+                    blackCount = 0;
+                }
+                if (redCount > 0){
+                    colNumbers += redCount +"R ";
+                    redCount = 0;
                 }
             }
-            if (count > 0) {
-                rowNumbers += count; // In case the last element is not non-zero.
-            }
-            if (rowNumbers.isEmpty()) {
-                rowNumbers = "0";
-            }
-            rowNumber[i] = rowNumbers;
         }
-        return rowNumber;
-    }
-    public static String[] getColumnNumbers(int[][] solutionNono) {
-        String[] colNumber = new String[solutionNono.length];
-        for (int j = 0; j < solutionNono[0].length; j++) {
-            String colNumbers = "";
-            int count = 0;
-            for (int i = 0; i < solutionNono.length; i++) {
-    
-                if (solutionNono[i][j] != 0) {
-                    count++;
-                } else {
-                    if (count > 0) {
-                        colNumbers += count +" ";
-                        count = 0;
-                    }
-                }
-            }
-            if (count > 0) {
-                colNumbers += count;
-            }
-            if (colNumbers.isEmpty()) {
-                colNumbers = "0";
-            }
-            colNumber[j] = colNumbers;
+        if (blackCount > 0) {
+            colNumbers += blackCount + "B";
+        } else if (redCount > 0) {
+            colNumbers += redCount + "R";
         }
-        return colNumber;
+        if (colNumbers.isEmpty()) {
+            colNumbers = "0";
+        }
+        colNumber[j] = colNumbers;
     }
-    
+    return colNumber;
+}
 }
