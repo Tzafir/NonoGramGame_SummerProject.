5349157141d916a803c3c8d354d2768c6fbb539a
Tzafir <168145165+Tzafir@users.noreply.github.com>
2024-05-17 13:52:48 +0100

    Final Solution

--- Nonograms/src/NonogramApp.java
+++ Nonograms/src/NonogramApp.java
@@ -3,11 +3,11 @@ import javax.swing.SwingUtilities;
 public class NonogramApp {
     public static void main(String[] args){
         int[][] solutionNono = {
-            {1, 0, 0, 1, 0},
-            {1, 0, 0, 1, 0},
-            {0, 0, 0, 1, 0},
-            {1, 0, 0, 1, 0},
-            {1, 0, 0, 1, 0},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 0, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 1, 0, 1},
+            {1, 2, 2, 2, 2},
         };
         SwingUtilities.invokeLater(() -> {
             new NanogramGrid(solutionNono);
