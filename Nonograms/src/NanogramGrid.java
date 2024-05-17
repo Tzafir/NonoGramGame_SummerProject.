0e1990d7b1288cb6d0bdad1346dd4fe66b194794
Tzafir <168145165+Tzafir@users.noreply.github.com>
2024-05-17 04:20:40 +0100

    Removed getrow and get column (into another file)

--- Nonograms/src/NanogramGrid.java
+++ Nonograms/src/NanogramGrid.java
@@ -13,60 +13,14 @@ public class NanogramGrid extends JFrame {
     private boolean testSolution(){
         return NonogramSolverCheck.testSolution(solutionNono, gridButtons);
     }
-
     private String[] getRowNumbers(int[][] solutionNono) {
-        String[] rowNumber = new String[solutionNono.length];
-        for (int i = 0; i < solutionNono.length; i++) {
-            String rowNumbers = "";
-            int count = 0;
-            for (int j = 0; j < solutionNono[i].length; j++) {
-
-                if (solutionNono[i][j] != 0) { // we Check that the element is non-zero and add it to the counter
-                    count++;
-                } else {
-                    if (count > 0) { // If the element is zero then we add the count to the variable rowNumbers.
-                        rowNumbers += count + " ";
-                        count = 0;
-                    }
-                }
-            }
-            if (count > 0) {
-                rowNumbers += count; // In case the last element is not non-zero.
-            }
-            if (rowNumbers.isEmpty()) {
-                rowNumbers = "0";
-            }
-            rowNumber[i] = rowNumbers;
-        }
-        return rowNumber;
+        return NonogramLabel.getRowNumbers(solutionNono);
     }
 
-    private String[] getColumnNumbers(int[][] solutionNano) {
-        String[] colNumber = new String[solutionNono.length];
-        for (int j = 0; j < solutionNono[0].length; j++) {
-            String colNumbers = "";
-            int count = 0;
-            for (int i = 0; i < solutionNono.length; i++) {
-
-                if (solutionNono[i][j] != 0) {
-                    count++;
-                } else {
-                    if (count > 0) {
-                        colNumbers += count +" ";
-                        count = 0;
-                    }
-                }
-            }
-            if (count > 0) {
-                colNumbers += count;
-            }
-            if (colNumbers.isEmpty()) {
-                colNumbers = "0";
-            }
-            colNumber[j] = colNumbers;
-        }
-        return colNumber;
+    private String[] getColumnNumbers(int[][] solutionNono) {
+        return NonogramLabel.getColumnNumbers(solutionNono);
     }
+
 
     // constructor
     public NanogramGrid(int[][] solution) {
